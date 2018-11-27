49e844c9f4c53bb02bad8ce524ceffa9a5192a43
beignetBanane <pascal.bassonville@gmail.com>
2018-11-27 22:32:40 +0400

    when loging in, return the error or the token id

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -2,25 +2,23 @@
 
 module.exports = function(app) {
   var {CrcmUser} = app.models;
+
   app.post('/login', function(req, res) {
-    console.log('login', req);
-    // console.log('login', req.body.email, req.body.password);
     CrcmUser.login({
       email: req.body.email,
       password: req.body.password,
     }, 'user', function(err, token) {
-      if (err) {
-        console.log('login failed', err);
-      }
+      if (err) return res.send(err);
+      let resp = {id: token.id};
+      res.send(resp);
     });
   });
 
   app.get('/logout', function(req, res) {
     if (!req.accessToken) return res.sendStatus(401);
     CrcmUser.logout(req.accessToken.id, function(err) {
-      if (err) {
-        console.log('logout failed', err);
-      }
+      if (err) return res.send(err);
+      res.sendStatus(200);
     });
   });
 };
